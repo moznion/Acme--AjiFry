d293e34e5d4c24fc62e24a542549deaf977f69a2
moznion <moznion@gmail.com>
2012-10-31 15:49:04 +0900

    Changed description of version. (v0.04 -> v0.05)

--- lib/Acme/AjiFry.pm
+++ lib/Acme/AjiFry.pm
@@ -9,7 +9,7 @@ use Encode;
 use List::Util;
 use base 'Class::Accessor::Fast';
 
-use version; our $VERSION = '0.04';
+use version; our $VERSION = '0.05';
 
 our %cols;
 our %rows;
@@ -315,7 +315,7 @@ Acme::AjiFry - AjiFry Language (アジフライ語) Translator
 
 =head1 VERSION
 
-This document describes Acme::AjiFry version 0.0.4
+This document describes Acme::AjiFry version 0.0.5
 
 
 =head1 SYNOPSIS
